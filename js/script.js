8f3c683c37ea3b7ff1c0920c2558114953a7b1a4
Michał Stankowski <monster26k@gmail.com>
2021-12-02 15:47:15 +0100

    fix js with ESLint

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
 'use strict';
 
-function titleClickHandler(event){
+function titleClickHandler(){
   const clickedElement = this;
   console.log('Link was clicked!');
 
   const activeLinks = document.querySelectorAll('.titles a.active');
-  const chceckActiveList = clickedElement.classList.contains('active');
+  const checkActiveList = clickedElement.classList.contains('active');
 
   for(let activeLink of activeLinks){
     activeLink.classList.remove('active');
   }
   
-  if (chceckActiveList){
+  if (checkActiveList){
     clickedElement.classList.remove('active');
   }else{
     clickedElement.classList.add('active');
@@ -19,19 +19,17 @@ function titleClickHandler(event){
 
   console.log('clickedElement:', clickedElement);
 
-  /* get 'href' attribute from the clicked link */
-  const clickedElementHref = clickedElement.getAttribute("href");
-  /* find the correct article using the selector (value of 'href' attribute) */
+
+  const clickedElementHref = clickedElement.getAttribute('href');
   const articleID = document.querySelector(clickedElementHref);
-  /* add class 'active' to the correct article */
   const activePosts = document.querySelectorAll('.post');
   
-  const chceckActivePost = articleID.classList.contains('active');
+  const checkActivePost = articleID.classList.contains('active');
   for(let activePost of activePosts){
     activePost.classList.remove('active');
   }
 
-  if (chceckActivePost){
+  if (checkActivePost){
     articleID.classList.remove('active');
   }else{
     articleID.classList.add('active');
@@ -45,16 +43,12 @@ const optArticleSelector = '.post',
 function generateTitleLinks(){
   const list = document.querySelector(optTitleListSelector);
   const articles = document.querySelectorAll(optArticleSelector);
-  /*let html = '';*/
   list.innerHTML = ''; 
 
   for(let article of articles){
     const articleID = article.id;
     const title = article.querySelector(optTitleSelector).innerHTML;
     list.innerHTML += '<li><a href="#'+articleID+'"><span>'+title+'</span></a></li>';
-    /*const  linkHTML = '<li><a href="#'+articleID+'"><span>'+title+'</span></a></li>';
-    html = html + linkHTML;
-    list.innerHTML = html;*/
   }
 }
 
